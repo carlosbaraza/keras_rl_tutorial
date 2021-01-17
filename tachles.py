eb71e7b7208047816330bd45c3cfe659a88b4d05
Carlos Baraza <carlos@baraza.io>
2021-01-17 00:56:30 +0000

    Update tachles.py

--- tachles.py
+++ tachles.py
@@ -5,7 +5,7 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
-tf.compat.v1.logging(tf.logging.ERROR)
+tf.compat.v1.logging(tf.compat.v1.logging.ERROR)
 
 
 import keras
